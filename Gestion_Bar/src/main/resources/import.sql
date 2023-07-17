552826ee2418a52284d988234a4d6d74539dc340
Bryan Orellana <bryan.orellana.est@tecazuay.edu.ec>
2023-07-17 11:07:35 -0500

    First Try

--- Gestion_Bar/src/main/resources/import.sql
+++ Gestion_Bar/src/main/resources/import.sql
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
-=======
+INSERT INTO istabar.personas
+(id_persona, apellido, cedula, correo, direccion, fechanacimiento, nombre, telefono) VALUES
+(1,'Suarez','3433','pc@gmail.com','Av. gonzalez','07/05/2020','Klever','43234'),
+(2,'Yunga','0987','mich@gmail.com','Ricaurte','07/05/2020','Michelle','0998765');
 
-INSERT INTO `personas`(`id_persona`, `apellido`, `cedula`, `correo`, `direccion`, `fechanacimiento`, `nombre`, `telefono`) VALUES (1,'Suarez','3433','pc@gmail.com','Av. gonzalez','07/05/2020','Klever','43234');
-INSERT INTO `personas`(`id_persona`, `apellido`, `cedula`, `correo`, `direccion`, `fechanacimiento`, `nombre`, `telefono`) VALUES (2,'Yunga','0987','mich@gmail.com','Ricaurte','07/05/2020','Michelle','0998765');
 
-
-INSERT INTO `administradores`(`id_administrador`, `contrase`, `usuario`, `id_persona`) VALUES (1,1,'1234','pool');
-INSERT INTO `administradores`(`id_administrador`, `contrase`, `usuario`, `id_persona`) VALUES (2,2,'michelle','michu');
->>>>>>> branch 'main' of https://github.com/MichelleYunga/Gestion_Bar.git
+INSERT INTO istabar.administradores (id_administrador, id_persona, contrase, usuario) VALUES
+(1,1,'1234','pool'),
+(2,2,'michelle','michu');
